f0acb1f880f793a8181a0fce413dbfcb8ab00ea3
Tharun1718 <tharunmano1170@gmail.com>
2022-12-05 13:59:31 +0530

    connected to node

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+const API ="https://tharun-mobiles.onrender.com";
 
 function App() {
+  const [mobiles, setMobiles] = useState([])  
+
+  // fetch the data from the api 
+  useEffect(()=>{
+     fetch(`${API}/mobiles`)
+     .then((data)=>data.json())
+     .then((res)=>setMobiles(res))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className=".phone-list-container" >
+        {mobiles.map((mobile,index)=>(
+          <Phone key={index} mobile={mobile} />
+        ))}
+      </div>
     </div>
   );
+}
+
+function Phone({mobile}){
+  return(
+    <div className="phone-container">
+      <img src={mobile.img} alt={mobile.model} className="phone-picture"/>
+      <h3 className="phone-name">{mobile.model}</h3>
+      <p className="phone-company">{mobile.company}</p>
+    </div>
+  )
 }
 
 export default App;
